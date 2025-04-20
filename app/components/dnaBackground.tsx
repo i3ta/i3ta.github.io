0872314b126c954b5a7697c1736c64ebf3e598aa
aaronhung <chung62@gatech.edu>
2025-04-20 00:23:58 -0400

    Redesigned page title

--- app/components/dnaBackground.tsx
+++ app/components/dnaBackground.tsx
@@ -3,15 +3,15 @@ import { DNAParticles } from '@/models/dnaParticles';
 import { Canvas } from '@react-three/fiber';
 import { Bloom, EffectComposer } from '@react-three/postprocessing';
 import * as THREE from 'three';
-import { CurvedPlane } from './curvedPlane';
+import { CurvedPlane } from '../../src/models/curvedPlane';
 
 export const DNABackground = ({ cameraAngle }: { cameraAngle?: number }) => {
   return (
-    <div className="absolute w-screen h-screen top-0 right-0">
+    <div className="absolute !w-screen !h-screen top-0 right-0">
       <Canvas
         camera={{ fov: 45 }}
         onCreated={({ scene }) => {
-          scene.background = new THREE.Color('black');
+          scene.background = new THREE.Color(23, 23, 23);
           scene.fog = new THREE.Fog('black', 5, 15);
         }}
       >
@@ -36,7 +36,8 @@ export const DNABackground = ({ cameraAngle }: { cameraAngle?: number }) => {
           radius={100}
           angle={0.3}
         />
-        <pointLight position={[-1, 0, 0]} intensity={7} />
+        <pointLight position={[-1, 0, 1.5]} intensity={7} />
+        <pointLight position={[-1, 0, -2.5]} intensity={7} />
       </Canvas>
     </div>
   );
--- app/pages/main/main.tsx
+++ app/pages/main/main.tsx
@@ -6,44 +6,57 @@ import { useScrollAbsolute } from '~/hooks/useScrollAbsolute';
 import { useScale } from '~/hooks/useScale';
 
 export const Main = () => {
+  /* Title entrance animation */
   const titleRef = useRef<HTMLHeadingElement>(null);
   const controls = useAnimation();
-
   const handleViewportEnter = () => {
     controls.start({
-      x: 0,
+      y: 0,
       transition: { type: 'spring', stiffness: 100, damping: 20 },
     });
   };
+  useEnter(titleRef, handleViewportEnter, false);
 
-  useEnter(titleRef, handleViewportEnter);
+  const scrollY = useScrollAbsolute();
 
-  const scrollAbs = useScrollAbsolute();
-  const cameraAngle = useScale(scrollAbs, [0, 400], [2.8, 3.3]);
-
-  const { scrollY } = useScroll();
-  const opacity2 = useTransform(scrollY, [0, 400], [0, 1]);
+  /* Shrinking background animation */
+  const bgWidth = useScale(scrollY, [0, 400], [100, 40]);
+  const bgHeight = useScale(scrollY, [0, 400], [100, 90]);
+  const bgRight = useScale(scrollY, [0, 400], [0, 5]);
+  const bgTop = useScale(scrollY, [0, 400], [0, 5]);
+  const bgBorderRadius = useScale(scrollY, [0, 400], [0, 20]);
+  const bgScroll = useScale(scrollY, [450, 2000], [0, -1550]);
 
   return (
     <div className="relative w-screen flex flex-row justify-center pt-16">
       {/* Background layers */}
-      <div className="fixed top-0 h-screen w-screen bg-gradient-to-br from-neutral-800 to-neutral-950 -z-20" />
-      <div className="fixed h-screen w-full top-0 -z-10 right-0 overflow-hidden">
-        <DNABackground cameraAngle={2.8} />
+      <div className="fixed top-0 h-screen w-screen bg-gradient-to-br from-neutral-900 to-neutral-950 -z-20" />
+      <div
+        className="fixed h-screen -z-10 overflow-hidden border border-black shadow-black !shadow-lg"
+        style={{
+          width: `${bgWidth}vw`,
+          height: `${bgHeight}vh`,
+          right: `${bgRight}vw`,
+          top: `calc(${bgTop}vh + ${bgScroll}px)`,
+          borderRadius: `${bgBorderRadius}px`,
+        }}
+      >
+        <DNABackground cameraAngle={2.8} heightScale={bgHeight / 100} />
       </div>
 
       {/* Content */}
-      <div className="relative w-9/10 max-w-5xl top-42 overflow-hidden flex flex-col items-start gap-4">
-        <motion.h1
-          ref={titleRef}
-          initial={{ x: '-100%' }}
-          animate={controls}
-          onViewportEnter={handleViewportEnter}
-          viewport={{ once: true, amount: 0.1 }}
-        >
-          Hi, I&apos;m <b>Aaron</b>.
-        </motion.h1>
-
+      <div className="relative w-9/10 max-w-5xl top-36 flex flex-col items-start gap-8">
+        <div className="flex flex-col overflow-hidden">
+          <h2>Hi, I&apos;m</h2>
+          <motion.h1
+            ref={titleRef}
+            className="font-bold text-9xl leading-[1.2] bg-linear-to-r from-tblue to-tgreen text-transparent bg-clip-text"
+            initial={{ y: '100%' }}
+            animate={controls}
+          >
+            Aaron Hung
+          </motion.h1>
+        </div>
         <p className="w-1/2">
           I&apos;m a second year <b>computer science + biochemistry</b> student
           on the pre-med track at <b>Georgia Tech 🐝</b>. My passions are in
@@ -53,7 +66,7 @@ export const Main = () => {
 
         <div className="w-1 h-[300px]" />
 
-        <motion.p className="w-1/2" style={{ opacity: opacity2 }}>
+        <motion.p className="w-1/2">
           I&apos;m a second year <b>computer science + biochemistry</b> student
           on the pre-med track at <b>Georgia Tech 🐝</b>. My passions are in
           biotechnology, and I strive to leverage the power of modern computing
